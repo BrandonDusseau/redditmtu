0ee0133e94ab0d4e97565822dcadfedd468771f6
Brandon Dusseau <brandon@emcnair-225-3.resnet.mtu.edu>
2014-11-18 23:19:27 -0500

    Initial Xcode project commit

---
+++ redditmtu/redditmtu/redditmtu.h
@@ -0,0 +1,19 @@
+//
+//  redditmtu.h
+//  redditmtu
+//
+//  Created by Brandon Dusseau on 11/18/14.
+//  Copyright (c) 2014 cs4121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for redditmtu.
+FOUNDATION_EXPORT double redditmtuVersionNumber;
+
+//! Project version string for redditmtu.
+FOUNDATION_EXPORT const unsigned char redditmtuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <redditmtu/PublicHeader.h>
+
+
