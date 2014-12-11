b50852d68bdd06defdcc7b0593c46694351adc88
Brandon Dusseau <brandon@emcnair-225-3.resnet.mtu.edu>
2014-12-11 16:07:39 -0500

    Adding Alex's UI changes. Please use branches from this point on.

---
+++ redditmtu/redditmtu/APIController.swift
@@ -0,0 +1,50 @@
+//
+//  APIController.swift
+//  TableTest
+//
+//  Created by Alex Dinsmoor on 12/6/14.
+//  Copyright (c) 2014 Dinsmoor. All rights reserved.
+//
+
+import Foundation
+
+protocol APIControllerProtocol {
+    func didReceiveAPIResults(results: NSDictionary)
+}
+
+class APIController{
+    
+    var delegate: APIControllerProtocol
+    
+    init(delegate: APIControllerProtocol) {
+        self.delegate = delegate
+    }
+    
+    func loadReddit() {
+        
+            let urlPath = "http://www.reddit.com/.json"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+                println("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+                if(err != nil) {
+                    // If there is an error parsing JSON, print it to the console
+                    println("JSON Error \(err!.localizedDescription)")
+                }
+                //let results: NSArray = jsonResult["results"] as NSArray
+                let results: NSArray = jsonResult["data"]!["children"] as NSArray
+                //let results: NSArray = results1["children"] as NSArray
+                self.delegate.didReceiveAPIResults(jsonResult)
+            })
+            
+            task.resume()
+        
+    }
+}
---
+++ redditmtu/redditmtu/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  TableTest
+//
+//  Created by Alex Dinsmoor on 12/7/14.
+//  Copyright (c) 2014 Dinsmoor. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    var post: Post?
+    
+    @IBOutlet weak var albumCover: UIImageView!
+    @IBOutlet weak var postAuthor: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = self.post?.title
+        postAuthor.text = self.post?.author
+        //albumCover.image = UIImage(data: NSData(contentsOfURL: NSURL(string: self.album!.largeImageURL)!)!)
+    }
+}
