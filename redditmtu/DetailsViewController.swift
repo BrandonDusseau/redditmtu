4562c0e3e97b07e11fc2ea6ab8ad63b279cd4831
Dinsmoor <ardinsmo@mtu.edu>
2014-12-17 17:28:22 -0500

    Commented Code

--- redditmtu/DetailsViewController.swift
+++ redditmtu/DetailsViewController.swift
@@ -18,18 +18,21 @@ class DetailsViewController: UIViewController, UITableViewDataSource, UITableVie
     
     let kCellIdentifier: String = "CommentCell"
     
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
     override func viewDidLoad() {
+        //Define post info labels
         titleLabel.text = self.post?.author
         postAuthor.text = self.post?.title
+        //API access
         api = APICommentController(delegate: self)
+        //Let user know data is being loaded
         UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-        
+        //The actual connection to the API and Reddit
         api!.loadPost("http://www.reddit.com"+self.post!.permaLink+".json");
-        
         self.title = self.post?.title;
     }
     
@@ -37,52 +40,56 @@ class DetailsViewController: UIViewController, UITableViewDataSource, UITableVie
         return comments.count
     }
     
+    //Creates a dynamic label for the comment
     func commentBody(text:String, width:CGFloat) -> UILabel{
         let label:UILabel = UILabel(frame: CGRectMake(10, 10, width, CGFloat.max))
         label.numberOfLines = 0
         label.lineBreakMode = NSLineBreakMode.ByWordWrapping
         label.text = text
-        
+        //Resizes label to text
         label.sizeToFit()
         return label
     }
     
+    //Creates dynamic label for posting user
     func commentPoster(text:String, width:CGFloat, offset:CGFloat) -> UILabel{
         let label:UILabel = UILabel(frame: CGRectMake(10, (10+offset), width, (10+offset)))
         label.numberOfLines = 0
         label.lineBreakMode = NSLineBreakMode.ByWordWrapping
         label.text = text
         label.font = UIFont(name: "Helvetica", size: 10.0)
+        //Resizes label to text
         label.sizeToFit()
         return label
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //Grab Cell
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
+        //Load Comment
         let comment = self.comments[indexPath.row]
+        //Create label, add to cell
         var commentText = commentBody(comment.body, width: 300.0)
         commentText.tag = 1
         commentText.layoutIfNeeded()
-        
-        
         cell.addSubview(commentText)
         
+        //Create label, add to cell
         var user = commentPoster(comment.author, width: 300.0, offset: commentText.frame.height)
         user.tag = 2
         user.layoutIfNeeded()
-       
-        
         cell.addSubview(user)
         
-        
+        //Dynamic cell height
         cell.sizeThatFits(commentText.frame.size)
         tableView.rowHeight = commentText.frame.height+10+user.frame.height
         cell.clipsToBounds = true
         
-        
         return cell
     }
     
+    //Function clears cell contents. This prevents the table from not clearing cell
+    //Contents when re-used
     func tableView(tableView: UITableView, didEndDisplayingCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(kCellIdentifier) as UITableViewCell
         cell.prepareForReuse()
@@ -90,8 +97,10 @@ class DetailsViewController: UIViewController, UITableViewDataSource, UITableVie
     
     // The APIControllerProtocol method
     func didReceiveAPIResults(results: NSArray) {
+        //JSON unwrapping
         let results1: NSDictionary = results[1] as NSDictionary
         let results2: NSArray = results1["data"]!["children"] as NSArray
+        //On a different thread, get comments
         dispatch_async(dispatch_get_main_queue(), {
             self.comments = Comment.commentsWithJSON(results2)
             self.appsTableView!.reloadData()
